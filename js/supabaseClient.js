20e2334ebfe1b2316a531e056f2d8f1f6602bcda
Maark92 <marcocassara92@hotmail.com>
2025-04-12 18:25:48 +0200

    Aggiornamento connessione Supabase e fix visualizzazione

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = 'https://cikjhhbwxobypgofavuj.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpa2poaGJ3eG9ieXBnb2ZhdnVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwNTQ2MjYsImV4cCI6MjA1OTYzMDYyNn0.t1tegLTbzqX75EUdo1BboKgudq6SggYkshOM-6d2oEo'
 
-export const supabaseClient = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
